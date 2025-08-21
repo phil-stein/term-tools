81f48bf01aeafbf5305fbaf46a7862acb428161d
Phil-Laptop <philipp033.25@gmail.com>
2025-08-21 18:03:52 +0200

    LAPTOP: worked on gitsync

---
+++ build_all.bat
@@ -0,0 +1,27 @@
+@echo off
+
+
+SET CUSTOM_ATTRIBUTES=-custom-attribute:NOTE -custom-attribute:TODO -custom-attribute:TMP -custom-attribute:BUG -custom-attribute:UNKNOWN
+
+del bin\gitsync.exe
+odin build gitsync -out:bin\gitsync.exe -vet-shadowing %CUSTOM_ATTRIBUTES%
+
+del bin\cat.exe
+odin build cat -out:bin\cat.exe -vet-shadowing %CUSTOM_ATTRIBUTES%
+
+del bin\notizn.exe
+odin build notizn -out:bin\notizn.exe -vet-shadowing %CUSTOM_ATTRIBUTES%
+
+del bin\ls.exe
+odin build ls -out:bin\ls.exe -vet-shadowing %CUSTOM_ATTRIBUTES%
+
+:: del bin\cpy.exe
+:: odin build cpy -out:bin\cpy.exe %CUSTOM_ATTRIBUTES%
+
+cd test
+gitsync
+cat name.txt
+notizn
+ls
+:: cpy
+cd ..
--- gitsync/main.odin
+++ gitsync/main.odin
@@ -1,8 +1,9 @@
 package gitsync
 
-import "core:fmt"
-import "core:os"
-import "core:c/libc"
+import     "core:fmt"
+import     "core:os"
+import     "core:c/libc"
+import win "core:sys/windows"
 
 // @TODO: do this in a config file 
 paths := [?]string{  
@@ -20,7 +21,12 @@ paths := [?]string{
 }
 
 main :: proc()
-{
+{ 
+  var_str : cstring16 = "%AppData%"
+  buf : [128]u16
+  ret := win.ExpandEnvironmentStringsW( var_str, raw_data(&buf), 128 )
+  if ret <= 0 { fmt.println( "[ERROR] win.ExpandEnvironmentStringsW failed with:", var_str ) }
+
   fmt.println( 
 ` M -> modified
 ?? -> untracked
